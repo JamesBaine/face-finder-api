b3022b173106b050ec5964812ad9e30ce801874c
JamesBaine <jamesmbaine@gmail.com>
2018-07-16 21:06:14 -0700

    user table error fixed in psql

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true
-    }
-  });
+      ssl: true,
+  }
+});
 
 const app = express();
 
